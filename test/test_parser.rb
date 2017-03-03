4a305b1e0583a8973f6ca275749ae4fba9c36e76
Sam Jewell <sam@bridge-u.com>
2017-03-03 11:56:11 +0000

    Add extra tests for parsing truncated JSON arrays

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -14,6 +14,36 @@ module RJSON
         '["foo",null,true]'
       ],
       [
+        'truncated_array',
+        '["foo",nul',
+        '["foo"]'
+      ],
+      [
+        'truncated_number_in_array',
+        '["foo",1',
+        '["foo"]'
+      ],
+      [
+        'truncated_array_ends_with_comma',
+        '["foo",',
+        '["foo"]'
+      ],
+      [
+        'truncated_array_ends_with_opening_square_bracket',
+        '["foo",[',
+        '["foo",[]]'
+      ],
+      [
+        'does_not_touch_uncorrupted_number_in_array',
+        '["foo",1]',
+        '["foo",1]'
+      ],
+      [
+        'truncated_value_in_nested_array',
+        '["foo",[10.3,["bar",fals',
+        '["foo",[10.3,["bar"]]]'
+      ],
+      [
         'object',
         '{"foo":{"bar":null}}',
         '{"foo":{"bar":null}}'
