0c012dff53c30f8330ac16746eeea342c8e67e48
Sam Jewell <sam@bridge-u.com>
2017-04-19 16:00:08 +0100

    Remove meta-programming and extract own matcher-method instead

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -7,109 +7,99 @@ require 'stringio'
 
 module RJSON
   class TestParser < MiniTest::Unit::TestCase
-    [
-      [
-        'array',
-        '["foo",null,true]',
-        '["foo",null,true]'
-      ],
-      [
-        'truncated_array',
-        '["foo",nul',
-        '["foo"]'
-      ],
-      [
-        'truncated_number_in_array',
-        '["foo",1',
-        '["foo"]'
-      ],
-      [
-        'truncated_array_ends_with_comma',
-        '["foo",',
-        '["foo"]'
-      ],
-      [
-        'truncated_array_ends_with_opening_square_bracket',
-        '["foo",[',
-        '["foo",[]]'
-      ],
-      [
-        'does_not_touch_untruncated_number_in_array',
-        '["foo",1]',
-        '["foo",1]'
-      ],
-      [
-        'truncated_value_in_nested_array',
-        '["foo",[10.3,["bar",fals',
-        '["foo",[10.3,["bar"]]]'
-      ],
-      [
-        'object',
-        '{"foo":{"bar":null}}',
-        '{"foo":{"bar":null}}'
-      ],
-      [
-        'truncated_object',
-        '{"foo":true,"bar":fals',
-        '{"foo":true}'
-      ],
-      [
-        'truncated_first_value_in_object',
-        '{"foo":tru',
-        '{}'
-      ],
-      [
-        'truncated_number_in_object',
-        '{"foo":13.5,"bar":1',
-        '{"foo":13.5}'
-      ],
-      [
-        'does_not_touch_untruncated_number_in_object',
-        '{"foo":13.5,"bar":1}',
-        '{"foo":13.5,"bar":1}'
-      ],
-      [
-        'truncated_value_in_nested_object',
-        '{"foo":13.5,"bar":{"baz":fals',
-        '{"foo":13.5,"bar":{}}'
-      ],
-      [
-        'truncated_object_key',
-        '{"foo":true,"ba',
-        '{"foo":true}'
-      ],
-      [
-        'truncated_object_ends_with_complete_key',
-        '{"foo":true,"bar"',
-        '{"foo":true}'
-      ],
-      [
-        'truncated_object_ends_with_colon',
-        '{"foo":true,"bar":',
-        '{"foo":true}'
-      ],
-      [
-        'truncated_object_ends_with_comma',
-        '{"foo":true,',
-        '{"foo":true}'
-      ],
-      [
-        'truncated_object_ends_with_opening_curly',
-        '{"foo":{',
-        '{"foo":{}}'
-      ],
-      [
-        'truncated_object_ends_with_decimal_point',
-        '{"foo":true,"foo_fraction":0.',
-        '{"foo":true}'
-      ]
-    ].each do |test_case|
-      define_method("test_#{test_case.first}") do
-        parser = new_parser test_case[1]
-        r = parser.parse.result
-        r_as_json = r.to_json
-        assert_equal(test_case.last, r_as_json)
-      end
+    def test_array
+      assert_parses_as('["foo",null,true]',
+                       '["foo",null,true]')
+    end
+
+    def test_truncated_array
+      assert_parses_as('["foo"]',
+                       '["foo",nul')
+    end
+
+    def test_truncated_number_in_array
+      assert_parses_as('["foo"]',
+                       '["foo",1')
+    end
+
+    def test_truncated_array_ends_with_comma
+      assert_parses_as('["foo"]',
+                       '["foo",')
+    end
+
+    def test_truncated_array_ends_with_opening_square_bracket
+      assert_parses_as('["foo",[]]',
+                       '["foo",[')
+    end
+
+    def test_does_not_touch_untruncated_number_in_array
+      assert_parses_as('["foo",1]',
+                       '["foo",1]')
+    end
+
+    def test_truncated_value_in_nested_array
+      assert_parses_as('["foo",[10.3,["bar"]]]',
+                       '["foo",[10.3,["bar",fals')
+    end
+
+    def test_object
+      assert_parses_as('{"foo":{"bar":null}}',
+                       '{"foo":{"bar":null}}')
+    end
+
+    def test_truncated_object
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,"bar":fals')
+    end
+
+    def test_truncated_first_value_in_object
+      assert_parses_as('{}',
+                       '{"foo":tru')
+    end
+
+    def test_truncated_number_in_object
+      assert_parses_as('{"foo":13.5}',
+                       '{"foo":13.5,"bar":1')
+    end
+
+    def test_does_not_touch_untruncated_number_in_object
+      assert_parses_as('{"foo":13.5,"bar":1}',
+                       '{"foo":13.5,"bar":1}')
+    end
+
+    def test_truncated_value_in_nested_object
+      assert_parses_as('{"foo":13.5,"bar":{}}',
+                       '{"foo":13.5,"bar":{"baz":fals')
+    end
+
+    def test_truncated_object_key
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,"ba')
+    end
+
+    def test_truncated_object_ends_with_complete_key
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,"bar"')
+    end
+
+    def test_truncated_object_ends_with_colon
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,"bar":')
+    end
+
+    def test_truncated_object_ends_with_comma
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,')
+    end
+
+    def test_truncated_object_ends_with_opening_curly
+      assert_parses_as('{"foo":{}}',
+                       '{"foo":{')
+    end
+
+    def test_truncated_object_ends_with_decimal_point
+      assert_parses_as('{"foo":true}',
+                       '{"foo":true,"foo_fraction":0.')
     end
 
     def test_invalid_but_not_trucated_json_raises_exception
@@ -118,6 +108,16 @@ module RJSON
       assert_raises(Racc::ParseError) do
         parser.parse.result
       end
+    end
+
+    private
+
+    def assert_parses_as(expected, actual)
+      parser = new_parser actual
+      assert_equal(
+        expected, parser.parse.result.to_json,
+        "Expected #{actual.inspect} to parse as #{expected.inspect}"
+      )
     end
 
     def new_parser string
